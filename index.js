f79b1f347e4a2aebd73503e114045d6964fdad0f
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-04-20 17:47:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// Write your code here!
+// find and remove the header
 document.querySelector("main#main").remove();
 
-
+// create h1 and assign it to variable
 const newHeader = document.createElement("h1");
+// give the h1 an id and content using that variable. 
 newHeader.id = "victory"
 newHeader.innerHTML = "Flatiron School is the champion!";
 newHeader.className = "victory";
